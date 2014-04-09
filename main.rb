fa135801096a019fa7dcdd493df01792ee5b8f31
Todd <toddjudderickson@gmail.com>
2014-04-08 18:58:46 -0600

    Finish player turn commit

--- main.rb
+++ main.rb
@@ -3,6 +3,57 @@ require 'sinatra'
 
 set :sessions, true
 
+helpers do
+  def calculate_total(cards)
+    arr = cards.map{|element| element[1]}
+
+    # add 1 for aces and 10 for face cards
+    total =0
+    arr.each do |a|
+      if a == "A"
+        total += 11
+      else
+        total += a.to_i == 0 ? 10 : a.to_i
+      end
+    end
+
+    # correct for aces
+    arr.select{|element| element == "A"}.count.times do
+      break if total <= 21
+      total -= 10
+    end
+
+    total
+  end
+
+  def card_image(card) # ['H', '4']
+
+    suit = case card[0]
+      when 'H' then 'hearts'
+      when 'D' then 'diamonds'
+      when 'C' then 'clubs'
+      when 'S' then 'spades'
+    end
+
+    value = card[1]
+    if ['J', 'Q', 'K', 'A'].include?(value)
+      value = case card[1]
+        when 'J' then 'jack'
+        when 'Q' then 'queen'
+        when 'K' then 'king'
+        when 'A' then 'ace'
+      end
+    end
+
+    "<img src='/images/cards/#{suit}_#{value}.jpg' class='card_image'>"
+  end
+end
+
+before do
+  @show_hit_or_stay_buttons = true
+end
+
+
 get '/' do
   if session[:player_name] 
     redirect '/game'
@@ -16,6 +67,12 @@ get '/new_player' do
 end
 
 post '/new_player' do
+
+  if params[:player_name].empty?
+    @error = "Please enter a name."
+    halt erb(:new_player)
+  end
+
   session[:player_name] = params[:player_name] 
   redirect '/game'
 end
@@ -36,6 +93,30 @@ get '/game' do
   session[:player_cards] << session[:deck].pop
 
   erb :game
-
-  # render game template
 end
+
+post '/game/player/hit' do
+  session[:player_cards] << session[:deck].pop
+  player_total = calculate_total(session[:player_cards])
+
+  if player_total == 21
+    @success = "Congratulations #{session[:player_name]}! You hit blackjack!"
+    @show_hit_or_stay_buttons = false
+
+  elsif player_total > 21
+    @error = "Sorry, #{session[:player_name]} busted."
+    @show_hit_or_stay_buttons = false
+
+  end
+
+  erb :game
+end
+
+post '/game/player/stay' do
+  @success = "#{session[:player_name]} chose to stay."
+  @show_hit_or_stay_buttons = false
+  erb :game
+end
+
+
+
